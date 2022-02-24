b5a7341b3ff860595578ad527aa6996be41548b1
Antonio Saiz <asaiz@paradigmadigital.com>
2022-02-24 10:21:26 +0100

    add: upload project from local

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+  return src("assets/scss/styles.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest("assets", { sourcemaps: "." }));
+}
+
+// JavaScript Task
+function jsTask() {
+  return src("assets/js/script.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("assets", { sourcemaps: "." }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("*.html", browsersyncReload);
+  watch(
+    ["assets/scss/**/*.scss", "assets/js/**/*.js"],
+    series(scssTask, jsTask, browsersyncReload)
+  );
+}
+
+// Default Gulp task
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Proyecto Gulp para maquetaciones rápidas
+
+Arquitectura de proyecto para maquetaciones rápidas con el automatizador de tareas gulp.
+
+Los pasos a seguir para dejar el gulp funcionando son los siguientes:
+
+Instalar [node.js](https://nodejs.org/es/)
+
+node -v
+
+npm -v
+
+npm install gulp-cli -g (si lo instalas por primera vez en tu equipo)
+
+npm install gulp gulp-sass gulp-postcss cssnano gulp-terser browser-sync
+
+[Website](www.antoniosaiz.com)
